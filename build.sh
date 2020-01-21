48f737fd71fca46348025c037427c3a8e0977db6
LeonMDev <Leonmeister97@gmail.com>
2020-01-21 14:01:36 +0100

    incorporate feedback from advisor as todos, some touchups

--- build.sh
+++ build.sh
@@ -3,19 +3,19 @@
 ## Usage: "sh build.sh Filename (Without extension)" -> e.g. "sh build.sh seminar" if the file is called seminar.tex
 #1. PDFLaTeX
 pdflatex  -synctex=1 -interaction=nonstopmode --src-specials $1.tex
-#2. Generieren der Bibliothek
+#2. generate library
 bibtex $1
-#3. Generieren des Abkürzungsverzeichnisses
+#3. generate list of abbreviations
 makeindex $1.nlo -s nomencl.ist -o $1.nls
-#4. Generieren des Glossars
+#4. generate glossary
 makeglossaries $1
-#5. Erneut PDFLaTeX (2mal!)
+#5. PDFLaTeX again
 pdflatex  -synctex=1 -interaction=nonstopmode --src-specials $1.tex
 pdflatex  -synctex=1 -interaction=nonstopmode --src-specials $1.tex
-#6. Erneutes Generieren des Abkürzungsverzeichnisses
+#6. generate abbreviations again
 makeindex $1.nlo -s nomencl.ist -o $1.nls
-#7. Ein letztes Mal PDFLaTeX
+#7. last PDFLaTeX
 pdflatex  -synctex=1 -interaction=nonstopmode --src-specials $1.tex
-#8. Temporäre Dateien entfernen
+#8. clean temp files
 find . -regextype posix-egrep -regex ".*\.(blg|aux|idx|ind|toc|lof|lot|mpx|tex1|txt1|undo|bbl|brf|ilg|log|lol|nlo|nls|out|bak|glg|glo|gls|glsdefs|ist)$" -type f -delete
 
